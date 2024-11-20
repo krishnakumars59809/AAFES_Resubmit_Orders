c06897978403109d7ca84257c5c35c5a0925120f
krishnakumarccx <krishna.kumar@changecx.com>
2024-11-20 20:45:58 +0530

    ECOMM-56465: useRandomAndEpochRRN flag skips RRN generation when set to true.

--- run.js
+++ run.js
@@ -1,4 +1,20 @@
-import { execute } from "./ResubmitOrder.js";
+import { execute } from "./src/ResubmitOrder.js";
+import { successOrders } from "./src/SuccessOrders.js";
 
-const orderNumbers = [1320122562,1320124342562]; // Example order numbers
-execute(orderNumbers)
+const orderNumbers = [
+  6720659981, 6720660052, 6720660169, 6720660283, 6720660460,
+  6720655869, 6720661188, 6720661589, 6720661869, 6720662495
+]; // Example order numbers
+
+const filteredOrderNumbers = orderNumbers.filter(orderNumber => {
+  return successOrders.some(order => order === orderNumber);
+});
+console.log("NUMBER OF ORDER NUMBERS:", orderNumbers?.length)
+console.log("NUMBER OF FILTERED ORDER NUMBERS:", filteredOrderNumbers?.length)
+
+if (filteredOrderNumbers?.length > 0) {
+  execute(filteredOrderNumbers)
+} else {
+  console.log("PLEASE TRY NEW ORDERS!!!!")
+}
+
